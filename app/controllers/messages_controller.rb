fa6e7dea54991c872fc2446f4a0126652a17bfa2
Hashy-Y <86712426+Hashy-Y@users.noreply.github.com>
2021-07-06 17:01:25 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = Message.includes(:user)
   end
 
   def new
@@ -17,6 +18,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end 
   end
